cf364e8c1f21f5f4b33d9d32e932a7adf555af9c
Robin Malhotra <malhotra.robin@yahoo.co.in>
2014-06-15 01:00:05 +0530

    Initial Commit

---
+++ parseWithCollectionViewController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  parseWithCollectionViewController
+//
+//  Created by Robin Malhotra on 15/06/14.
+//  Copyright (c) 2014 Robin's code kitchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
